d9857bbaf9710a70c0cf11a2d3e31c8d732d66f6
DW1AU17 <438981082@qq.com>
2020-03-12 16:40:12 +0800

    首页布局

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router/router'
+import ViewUI from 'view-design'   
+import 'view-design/dist/styles/iview.css'
+import '@/assets/styles/base.css'
+
+Vue.use(ViewUI)  // 整个引入
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
+
+
+// import { Button } from 'view-design'  // 按需引入
+// Vue.component('Button', Button);  // 按需引入
---
+++ src/pages/index/index.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="index">
+        <!-- 头部 / 上部 -->
+        <header class="header container">
+            1
+        </header>
+        <nav class="first-nav">
+            <div class="container">
+
+            </div>
+        </nav>
+        <div class="swiper">
+            2
+        </div>
+         <nav class="second-nav">
+            <div class="container">
+
+            </div>
+        </nav>
+        <!-- 主体 -->
+        <div class="main container">
+            3
+        </div>
+        <!-- 底部 -->
+        <footer class="foot">
+            <div class="container">
+                4
+            </div>
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {
+        
+    }
+}
+</script>
+
+<style scoped lang="less">
+.index {
+    .header {
+        height: 106px;
+        background-color: pink;
+    }
+    .first-nav {
+        height: 60px;
+        background-color: #d4cbbd;
+    }
+    .swiper {
+        height: 470px;
+        background-color: skyblue;
+    }
+    .second-nav {
+        height: 60px;
+        background-color: #f3bd66;
+    }
+    .main {
+        min-height: 300px;
+        background-color: tomato;
+    }
+    .foot {
+        height: 468px;
+        background-color: #585657;
+    }
+}
+
+</style>
